21505b4ed57481dc6827a7042a72650eccd63027
FabricioNascimentop <fabricio.nascimentop2024@gmail.com>
2025-04-27 21:35:29 -0300

    esqueleto da visualização do AutoCar pronto

--- src/sections/projetos.js
+++ src/sections/projetos.js
@@ -1,7 +1,7 @@
 import textos from '../textos.json'
 import React from 'react';
 import '../styles/projetos.css'
-
+import textos_projetos from '../textos_projetos.json'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -50,10 +50,10 @@ export function AutoCar(){
                 <AutoCarView page='/carros/Nissan%20Altima?id=1'/>
              </SwiperSlide>
               <SwiperSlide>
-                <AutoCarView page='/adicionar carro'/>
+                <AutoCarView page='/editar/Nissan Altima?id=1'/>
              </SwiperSlide>
               <SwiperSlide>
-                <AutoCarView page='/editar/Nissan Altima?id=1'/>
+                <AutoCarView page='adicionar carro'/>
              </SwiperSlide>
 
       </Swiper>
@@ -64,6 +64,19 @@ export function AutoCar(){
 }
 function AutoCarView({page}){
   const src = `https://autocar-zojn.onrender.com/${page}`
+  let sic
+  if(page == '/'){
+    sic = 'home'
+  }
+  else if(page == '/carros/Nissan%20Altima?id=1'){
+    sic = 'detalhes carro exemplo'
+  }
+  else if(page == '/editar/Nissan Altima?id=1'){
+    sic = 'editar carro exemplo'
+  }
+  else{
+    sic = page
+  }
   return(
         <div className='sliderViewer'>
 
@@ -72,6 +85,21 @@ function AutoCarView({page}){
           </div>
 
           <div className='container-project-ego'>
+              <div>
+                <h2>{sic}</h2>
+            <div>
+              <p>{textos_projetos['autocar'].pt[sic].i1}</p>
+            </div>
+              </div>
+            <div>
+              <h2 style={{textAlign:'left'}}>Descrição:</h2>
+              <h3>{textos_projetos['autocar'].pt[sic].i2}</h3>
+            </div>
+            <div>
+              <p>{textos_projetos['autocar'].pt[sic].i3}</p>
+              <br/>
+              <p>para melhor visualização do projeto completo clique <a href='https://autocar-zojn.onrender.com/login recrutador' target='_blank'>aqui</a></p>
+            </div>
           </div>
 
         </div>
